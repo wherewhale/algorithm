95d3b35c9ef73a315d1039038ef53676e024f071
wherewhale <syh980130@gmail.com>
2024-03-06 11:06:22 +0900

    feat(programmers): add solved problem

--- programmers/level2/도넛과_막대_그래프.js
+++ programmers/level2/도넛과_막대_그래프.js
@@ -1,43 +1,37 @@
-const isStartEdge = (edges) => {
-  const prevs = [];
-  const nexts = [];
-  var startEdge = undefined;
-
-  edges.forEach((i) => {
-    prevs.push(i[0]);
-    nexts.push(i[1]);
-  });
-
-  const complement = prevs.filter((x) => !nexts.includes(x));
-  // 여기서 중복된 배열 요소 검열
-
-  complement.forEach((x, index) => {
-    if (complement.indexOf(x, index + 1) !== -1) {
-      startEdge = x;
-      return;
-    }
-  });
-  return startEdge;
-};
-
 function solution(edges) {
-  var answer = [];
-  var graphStartEdges = []; // 시작 정점과 연결된 graph edge
+  var answer = [0, 0, 0, 0];
+  var graph = 0;
+  const edgeSet = new Set();
 
-  answer.push(isStartEdge(edges)); //시작점을 찾는 함수
+  edges.forEach((edge) => {
+    edgeSet.add(edge[0]);
+    edgeSet.add(edge[1]);
+  });
 
-  edges.forEach((x, index) => {
-    if (x[0] === answer[0]) {
-      graphStartEdges.push(x[1]);
-      edges.splice(index, 1); //불필요한 간선 제외
+  const inputs = Array.from({ length: edgeSet.size }, () => 0);
+  const outputs = Array.from({ length: edgeSet.size }, () => 0);
+
+  edges.forEach((edge) => {
+    inputs[edge[1] - 1]++;
+    outputs[edge[0] - 1]++;
+  });
+
+  outputs.forEach((output, index) => {
+    if (output === 2 && inputs[index] !== 0) {
+      answer[3]++;
+    } else if (output === 0) {
+      answer[2]++;
     }
   });
-  // 시작점과 관련된 간선 수, 정점 수 확인
-  // 다시 돌아오거나 간선이 끝나는 경우
-  // recursive function으로 구현
 
-  // 간선이 끝나는 경우 -> stick 형 + 1
-  // 다시 돌아오는 경우 -> 8자 or donut 형 + 1
+  inputs.forEach((input, index) => {
+    if (input === 0 && outputs[index] >= 2) {
+      answer[0] = index + 1;
+      graph = outputs[index];
+    }
+  });
+
+  answer[1] = graph - (answer[2] + answer[3]);
 
   return answer;
 }
